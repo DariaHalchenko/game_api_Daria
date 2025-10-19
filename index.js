ad5cfea94346148d29b77d5d2240c95bf85549a4
DariaHalchenko <daragalcenko3@gmail.com>
2025-10-19 19:32:54 +0300

    As an app developer I want to be able to add new games

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+
+app.use(express.json())
 
 const games = [
     {id: 1, name: "Witcher 3", price: 29.99},
@@ -18,9 +21,7 @@ const games = [
 app.get('/games', (req, res) => {
     res.send(games)
 })
-
 app.get('/games/:id', (req, res) => {
-
     if(typeof games[req.params.id - 1] === 'undefined') {
         return res.status(404).send({
             error: "Game not found"
@@ -29,8 +30,30 @@ app.get('/games/:id', (req, res) => {
     res.send(games[req.params.id - 1])
 })
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.post('/games', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({
+            error: 'One or all params are missing'
+        })
+    }
+    let game = {
+        id: games.length + 1,
+        price: req.body.price,
+        name: req.body.name
+    }
+    games.push(game)
+
+    res.status(201)
+        .location(`${getBaseUrl(req)}/games/${games.length}`)
+        .send(game)
+})
+app.use('/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+    ? 'https' : 'http' + `://${req.headers.host}`
+}
